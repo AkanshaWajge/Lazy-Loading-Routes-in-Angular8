f72d160cd9d1e39deb376edae58d024292e9a0c3
Akansha Wajge <akanshad.wajge@gmail.com>
2019-10-24 12:24:47 +0530

    Created 3 components : cart, confirm and checkout

--- LazyLoading/src/app/shop/shop.module.ts
+++ LazyLoading/src/app/shop/shop.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ConfirmComponent } from './confirm/confirm.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CartComponent, CheckoutComponent, ConfirmComponent],
   imports: [
     CommonModule
   ]

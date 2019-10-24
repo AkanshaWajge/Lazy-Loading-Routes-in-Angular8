a512be7c82e3e2d4ea0ef678089288b3ec9158b5
Akansha Wajge <akanshad.wajge@gmail.com>
2019-10-24 13:52:49 +0530

    RouterModule’s forChild method instead of forRoot in feature module
    n the feature module itself, you’ll include your routes with RouterModule’s forChild method instead of forRoot

--- LazyLoading/src/app/shop/shop.module.ts
+++ LazyLoading/src/app/shop/shop.module.ts
@@ -4,10 +4,15 @@ import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ConfirmComponent } from './confirm/confirm.component';
 
+import { routes } from './shop.routing';
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [CartComponent, CheckoutComponent, ConfirmComponent],
+  
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
 })
 export class ShopModule { }
